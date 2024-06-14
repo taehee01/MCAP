b1cee5ec1ce314d8bf5a160bb14cd8206b294c8e
taehee01 <taehui120115@gmail.com>
2024-06-14 19:36:45 +0900

    0614

--- app/src/main/java/com/example/mcap/MainActivity.java
+++ app/src/main/java/com/example/mcap/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.mcap;
 
+import android.content.Intent;
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,7 +13,8 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btnHost;
+    Button btnjoin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,6 +24,22 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        btnHost =findViewById(R.id.btnHost);
+        btnjoin=findViewById(R.id.btnJoin);
+        btnHost.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(), HostActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnjoin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(getApplicationContext(), ConnectActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
