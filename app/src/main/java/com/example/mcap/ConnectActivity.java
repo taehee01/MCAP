34378b798df1256eada46aff0f5b4be81326bd23
taehee01 <taehui120115@gmail.com>
2024-06-21 19:29:11 +0900

    0621

--- app/src/main/java/com/example/mcap/ConnectActivity.java
+++ app/src/main/java/com/example/mcap/ConnectActivity.java
@@ -1,6 +1,11 @@
 package com.example.mcap;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ConnectActivity extends AppCompatActivity {
-
+    Button btnConnect;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_connect);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        btnConnect = findViewById(R.id.btnConnect);
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), GuestActivity.class));
+            }
         });
     }
 }
--- app/src/main/java/com/example/mcap/GuestActivity.java
+++ app/src/main/java/com/example/mcap/GuestActivity.java
@@ -1,6 +1,10 @@
 package com.example.mcap;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class GuestActivity extends AppCompatActivity {
-
+    TextView textJoinIP;
+    TextView textMessageLog;
+    EditText editGuestName;
+    EditText editInput;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_guest);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        textJoinIP = findViewById(R.id.JoinIP);
+        textMessageLog = findViewById(R.id.textGuestMessage);
+        editGuestName = findViewById(R.id.editGuestName);
+        editInput = findViewById(R.id.editGuestInput);
+        editInput.setOnKeyListener(new View.OnKeyListener() {
+            @Override
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if(event.getKeyCode() == KeyEvent.KEYCODE_ENTER && event.getAction() == KeyEvent.ACTION_DOWN){
+                    String nick = editGuestName.getText().toString();
+                    String message = editInput.getText().toString();
+                    String log = nick + " : " + message;
+                    String beforeLog = textMessageLog.getText().toString();
+                    beforeLog = beforeLog + "\n " + log;
+                    textMessageLog.setText(beforeLog);
+                    editInput.setText("");
+                }
+                return false;
+            }
         });
     }
 }
